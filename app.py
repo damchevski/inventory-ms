cbf187524f44edb680c1189b092f260e86ec2892
damchevski <42002981+damchevski@users.noreply.github.com>
2021-03-29 17:28:05 +0200

    review

--- app.py
+++ app.py
@@ -6,12 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 def get_test1(test1_id):
     return {'id': 1, 'name': 'name', 'entered_id': test1_id}
 
-
 def person_add(person_body):
     new_person = Person(name=person_body['name'], surname=person_body['surname'])
     db.session.add(new_person)
     db.session.commit()
-
 
 def person_find(person_name):
     found_person = db.session.query(Person).filter_by(name=person_name).first()
@@ -29,8 +27,7 @@ migrate = Migrate(app, db)
 connexion_app.add_api("api.yml")
 
 # dummy reference for migrations only
-from models import User, Person
-from models2 import Person2
+from models import Person
 
 if __name__ == "__main__":
     connexion_app.run(host='0.0.0.0', port=5000, debug=True)
--- models.py
+++ models.py
@@ -1,11 +1,5 @@
 from app import db
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-
-
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False)
